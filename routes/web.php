d093916b14a18a55e305dd2b653a25215e22b952
vagrant <vagrant@homestead>
2017-09-17 14:30:03 +0000

    添加了几个主页的路由

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/','StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
