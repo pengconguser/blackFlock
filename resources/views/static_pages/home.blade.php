3be705e9a8e8991681f4c19f4af6b3ab5df25544
peng <531299585@qq.com>
2017-10-18 10:14:46 +0800

    恢复了主页报错的情况

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -12,10 +12,10 @@
       </div>
       <aside class="col-md-4">
         <section class="user_info">
-          @include('shared._user_info', ['user' => Auth::users()])
+          @include('shared._user_info', ['user' => Auth::user()])
         </section>
         <section class="stats">
-          @include('shared._stats', ['user' => Auth::users())])
+          @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
