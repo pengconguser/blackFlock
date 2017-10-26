e87696b6f0df661c2518da03784bbd52cf8ef50f
peng <531299585@qq.com>
2017-10-26 11:13:11 +0800

    修改了添加动态时会自动回话提醒

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,39 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Auth;
 use App\Status;
+use Auth;
+use Illuminate\Http\Request;
 
-class StatusesController extends Controller
-{
-    public function __construct()
-    {
-    	$this->middleware('auth');
-    }
+class StatusesController extends Controller {
+	public function __construct() {
+		$this->middleware('auth');
+	}
 
-    public function store(Request $request)
-    {
-       $this->validate($request,[
-          'content'=>'required|max:140'
-       ]);
+	public function store(Request $request) {
+		$this->validate($request, [
+			'content' => 'required|max:140',
+		]);
 
-       Auth::user()->statuses()->create([
-          'content'=>$request->content
-       ]);
+		Auth::user()->statuses()->create([
+			'content' => $request->content,
+		]);
 
-       session()->flash('status','aaaa');
+		session()->flash('success', '添加成功!');
 
-       return redirect()->back();
-    }
+		return redirect()->back();
+	}
 
-    public function destroy($id)
-    {
-      $status=Status::find($id);
-      if($status){
-        $status->delete();
-        session()->flash('success','删除成功!');
-        return redirect()->back();
-      }
-    }
+	public function destroy($id) {
+		$status = Status::find($id);
+		if ($status) {
+			$status->delete();
+			session()->flash('success', '删除成功!');
+			return redirect()->back();
+		}
+	}
 }
