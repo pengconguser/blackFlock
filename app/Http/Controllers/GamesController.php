220e50dd40cf25e72c7617acd923d14a9c51a3dc
peng <531299585@qq.com>
2017-10-27 17:07:54 +0800

    增加了一个中间件,不登录不允许玩小游戏

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -7,6 +7,9 @@ use Auth;
 use Illuminate\Http\Request;
 
 class GamesController extends Controller {
+	public function __construct() {
+		$this->middleware('auth');
+	}
 	public function index() {
 		$user = Auth::user();
 		return view('five')->withUser($user);
