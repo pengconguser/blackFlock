20def310eaac434bbd26c505decdc0ba47433109
peng <531299585@qq.com>
2017-11-13 16:54:24 +0800

    修复安全隐患

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,10 +3,11 @@
   <head>
     <title>@yield('title', '黑白路') - composer	</title>
     <link rel="stylesheet" href="/css/app.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
   </head>
   <body>
     @include('layouts._header')
-
+<div id="app">
     <div class="container">
       <div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
@@ -14,6 +15,7 @@
         @include('layouts._footer')
       </div>
     </div>
+</div>
 {{--     <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  --}}
     <script src="/js/app.js"></script>
