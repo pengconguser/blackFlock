c91666a57412d7512b513841416f16be6a3a3da0
peng <531299585@qq.com>
2017-12-15 23:13:14 +0800

    修整了一下前段的样式

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -14,39 +14,34 @@
         </div>
         <div class="panel-body">
             @include('shared._errors')
-
-
-        	{!! Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'form-horizontal']) !!}
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                {!! Form::label('email', '邮箱') !!}
-        	        {!! Form::text('email', null, ['class' => 'form-control', 'required' => 'required']) !!}
-                <small class="text-danger">
-                    {{ $errors->first('email') }}
-                </small>
-            </div>
-{{--             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                       {!! Form::label('password', '密码') !!}
-                      {!! Form::text('password', null, ['class' => 'form-control', 'required' => 'required']) !!}
-                <small class="text-danger">
-                    {{ $errors->first('password') }}
-                </small>
-            </div> --}}
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                {!! Form::label('password', '密码') !!}
+            <div class="col-md-12">
+                {!! Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'form-horizontal']) !!}
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    {!! Form::label('email', '邮箱') !!}
+                    {!! Form::text('email', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                    <small class="text-danger">
+                        {{ $errors->first('email') }}
+                    </small>
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    {!! Form::label('password', '密码') !!}
                 {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
-                <small class="text-danger">{{ $errors->first('password') }}</small>
+                    <small class="text-danger">
+                        {{ $errors->first('password') }}
+                    </small>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input name="remember" type="checkbox">
+                            记住我
+                        </input>
+                    </label>
+                </div>
+                <div class="btn-group pull-right">
+                    {!! Form::submit("登录", ['class' => 'btn btn-success']) !!}
+                </div>
+                {!! Form::close() !!}
             </div>
-            <div class="checkbox">
-                <label>
-                    <input name="remember" type="checkbox">
-                        记住我
-                    </input>
-                </label>
-            </div>
-            <div class="btn-group pull-right">
-                {!! Form::submit("登录", ['class' => 'btn btn-success']) !!}
-            </div>
-            {!! Form::close() !!}
             <hr>
                 <p>
                     还没账号？
