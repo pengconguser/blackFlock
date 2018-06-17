9377fb1a36f3c206fa00237a87b0194a85846d72
peng <531299585@qq.com>
2018-06-17 20:48:18 +0800

    去掉hhvm 的laravel swoole拓展包

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
 		Illuminate\Validation\ValidationServiceProvider::class,
 		Illuminate\View\ViewServiceProvider::class,
 		Intervention\Image\ImageServiceProvider::class,
- 		Hhxsv5\LaravelS\Illuminate\LaravelSServiceProvider::class,
+ 		// Hhxsv5\LaravelS\Illuminate\LaravelSServiceProvider::class,
 		/*
 			         * Package Service Providers...
 		*/
