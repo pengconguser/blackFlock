5ee74804690f0b0e7279a7cba5189d8371cb2e98
peng <531299585@qq.com>
2018-06-18 00:32:56 +0800

    提交配置,当php进入事件循环值周调用websoket服务器

---
+++ routes/websocket.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use SwooleTW\Http\Websocket\Facades\Websocket;
+
+/*
+|--------------------------------------------------------------------------
+| Websocket Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register websocket events for your application.
+|
+*/
+
+Websocket::on('connect', function ($websocket, Request $request) {
+    // called while socket on connect
+    $websocket->emit('websocket start');
+});
+
+Websocket::on('disconnect', function ($websocket) {
+    // called while socket on disconnect
+    $websocket->emit('websocket close');
+});
+
+Websocket::on('example', function ($websocket, $data) {
+    $websocket->emit('message', $data);
+});
+
+// Websocket::on('test', 'ExampleController@method');
